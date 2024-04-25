60cf05335baf80b22610889e3c1ca9842f56a66a
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 15:06:56 +0300

    Remove backend

--- backend.tf
+++ backend.tf
@@ -1,13 +1,3 @@
 /**********************************************************
   # Configure S3 Backend
 **********************************************************/
-
-terraform {
-  backend "s3" {
-    bucket         = "devops-ci-cd-12345678"
-    key            = "devops-ci-cd-12345678.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "devops-ci-cd-12345678"
-    profile        = "default"
-  }
-}
