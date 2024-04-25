b87283284713416e245621666b783206f16d3972
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 15:13:24 +0300

    Update bucket and dynamodb tables

--- backend.tf
+++ backend.tf
@@ -4,8 +4,8 @@
 
 terraform {
   backend "s3" {
-    bucket         = "devops-ci-cd-12345678"
-    key            = "devops-ci-cd-12345678.tfstate"
+    bucket         = "devops-ci-cd-12345678910"
+    key            = "devops-ci-cd-12345678910.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "devops-ci-cd-12345678"
   }
