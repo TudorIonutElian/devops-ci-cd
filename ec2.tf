294cfac413e0d91f6282192884035c5cb05be6e5
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 19:51:17 +0300

    Fix key name

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@
 resource "aws_instance" "teamcity_ec2_instance" {
   ami           = data.aws_ami.teamcity_ami_filter.id
   instance_type = var.teamcity_instance_type
-  key_name      = var.teamcity_key_name 
+  key_name      = aws_key_pair.kp.key_name
   user_data     = file("scripts/teamcity_entry.sh")
 
   tags = {
