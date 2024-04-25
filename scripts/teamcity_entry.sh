0c46509f67b892253cd5fb72d313badc72ad2de6
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 18:08:47 +0300

    Update entry

--- scripts/teamcity_entry.sh
+++ scripts/teamcity_entry.sh
@@ -9,6 +9,11 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+source ~/.bashrc 
+
 sudo mkdir teamcity
 cd ~/teamcity
 sudo mkdir data
@@ -40,7 +45,6 @@ volumes:
   logs:
   agent:
 EOF
-
 
 sudo docker-compose build
 sudo docker-compose up -d
