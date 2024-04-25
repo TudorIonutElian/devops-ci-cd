d6201a6b95cb05014f82a97ffe3795794bc7f1c7
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 15:07:42 +0300

    Add s3 back

--- backend.tf
+++ backend.tf
@@ -1,3 +1,12 @@
 /**********************************************************
   # Configure S3 Backend
 **********************************************************/
+
+terraform {
+  backend "s3" {
+    bucket         = "devops-ci-cd-12345678"
+    key            = "devops-ci-cd-12345678.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "devops-ci-cd-12345678"
+  }
+}
