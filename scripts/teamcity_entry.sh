bb9aa4a4980e5d6080cf100a47340ab4f0e850ec
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-04-25 18:07:54 +0300

    Update entry script

--- scripts/teamcity_entry.sh
+++ scripts/teamcity_entry.sh
@@ -4,17 +4,43 @@
 yum update -y
 yum install -y docker
 yum install -y git
-yum install -y docker-compose
+yum install -y nano
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 
 sudo mkdir teamcity
-cd teamcity
+cd ~/teamcity
 sudo mkdir data
 sudo mkdir logs
 sudo mkdir agent
 
-cd teamcity
-sudo docker compose build
-sudo docker compose up -d
+sudo cat <<EOF > docker-compose.yml
+version: "3"
+services:
+  tcs:
+    image: jetbrains/teamcity-server
+    container_name: teamcity-server
+    ports:
+      - "8111:8111"
+    volumes:
+      - data:/data/teamcity_server/datadir
+      - logs:/opt/teamcity/logs
+  tca:
+    image: jetbrains/teamcity-agent
+    depends_on:
+      - tcs
+    container_name: teamcity-agent
+    environment:
+      SERVER_URL: "http://tcs:8111/"
+    volumes:
+      - agent:/data/teamcity_agent/conf
+volumes:
+  data:
+  logs:
+  agent:
+EOF
+
+
+sudo docker-compose build
+sudo docker-compose up -d
